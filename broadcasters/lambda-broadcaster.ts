3f720d3b201aa2496c53cc578dc5fb526270b2db
hatskier <suvorovalex1997@gmail.com>
2021-03-10 01:23:28 +0100

    lambda broadcaster updated

--- broadcasters/lambda-broadcaster.ts
+++ broadcasters/lambda-broadcaster.ts
@@ -5,16 +5,7 @@ const PRICES_EP_URL = "https://api.limestone.finance/prices";
 
 const lambdaBroadcaster: Broadcaster = {
   async broadcast(prices: PriceDataSigned[]): Promise<void> {
-
-    // TODO: implement lambda endpoint for bulk prices uploading
-    // await axios.post(PRICES_EP_URL, {
-    //   prices,
-    //   permawebTx,
-    // });
-
-    for (const price of prices) {
-      await axios.post(PRICES_EP_URL, price);
-    }
+    await axios.post(PRICES_EP_URL, prices);
   },
 };
 
